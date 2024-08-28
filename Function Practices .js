25a16859a882155de2a9ccdbf0bf630e4289749c
Manoj Kumar <125989430+manojkumarcrazy@users.noreply.github.com>
2024-08-28 09:11:23 +0530

    font update

---
+++ Function Practices .js
@@ -0,0 +1,11 @@
+////
+
+///////  They are four types of function 
+
+// 1. Function Decleration
+// 2. Function Expression 
+// 3. Arrow Function
+// 4. Anonymous Function 
+
+
+// 1.Function Decleration
